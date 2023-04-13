091159efafef005dd8df567d9ba55b1d88aa076f
Daniela Estupinan <daniela.estupinan@udla.edu.ec>
2023-04-13 00:19:50 -0500

    Database

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
+require('dotenv').config();
 const express = require ('express');
 const app = express();
-const PORT = process.env.PORT || 4111;
+const PORT = process.env.PORT || 8080;
 const mongoose = require('mongoose');
 
 app.listen(PORT, console.log("Server start for port: "+ PORT))
 
-//database connection
 
+//database connection
 mongoose.connect(process.env.DB_URI, {
     useNewUrlParser : true,
-    useUnifiedTopology : true,
-    useFindAndModify: true,
-    useCreateIndex : true
+    useUnifiedTopology : true
 }).then(()=>console.log('Connected to the database')).catch(err=> console.log(err));
+
